b6ca81caa379cc5d4a9fb369f213d6def063f9ea
cjohnosn394 <59090049+cjohnosn394@users.noreply.github.com>
2021-08-20 10:19:21 -0400

    Add files via upload

---
+++ Craig_Johnson_Code_Demo - Copy/Craig_Johnson_Code_Demo/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Craig_Johnson_Code_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        /// 
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(441, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find Sequence";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(180, 479);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(767, 50);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(561, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Type a natural number less than 715827882 in the box, then, click the button to s" +
+    "ee the Collatz sequence generated. ";
+            // 
+            // chart1
+            // 
+            this.chart1.AllowDrop = true;
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.HorizontalCenter;
+            this.chart1.BackImageTransparentColor = System.Drawing.Color.White;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.AxisX.Title = "Step";
+            chartArea1.AxisY.Title = "Value";
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.BackImageTransparentColor = System.Drawing.Color.White;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.CursorX.IsUserEnabled = true;
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.CursorY.IsUserEnabled = true;
+            chartArea1.CursorY.IsUserSelectionEnabled = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(180, 87);
+            this.chart1.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chart1.Size = new System.Drawing.Size(767, 400);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label2
+            // 
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 520);
+            this.label2.TabIndex = 6;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(856, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(959, 535);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Craig_Johnson_Code_Demo - Copy/Craig_Johnson_Code_Demo/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace Craig_Johnson_Code_Demo
+{
+    public partial class Form1 : Form
+    {
+        /// prevents integer overflow
+        private const int MaxInt = 715827882;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// only procceeds if the entered value is a positive digit
+            int value;
+            if (int.TryParse(textBox2.Text, out value))
+            {
+                var steps = 0;
+                var sequence = new List<int>();
+                var status = 0;
+                int number = Int32.Parse(textBox2.Text);
+
+                if (number > 0)
+                {
+                    /// refreshes dataset used to graph
+                    sequence.Add(number);
+                    chart1.Series.Clear();
+                    chart1.Series.Add("Series1");
+                    chart1.Series[0].LegendText = "cycle for " + number.ToString();
+                    chart1.Series[0].ChartType = SeriesChartType.Line;
+                    chart1.AutoSize = true;
+
+                    while (status != 1)
+                    {
+                        steps += 1;
+                        if (number == 1)
+                        {
+                            status = 1;
+                            sequence.Add(4);
+                            sequence.Add(2);
+                            sequence.Add(1);
+                        }
+                        else if (number % 2 == 0)
+                        {
+                            number /= 2;
+                            sequence.Add(number);
+                        }
+                        else
+                        {
+                            if (MaxInt > number)
+                            {
+                                number = number * 3 + 1;
+                                sequence.Add(number);
+                            }
+                            else
+                            {
+                                ///catches any possible overflow not caught previously--perhaps unneeded
+                                throw new Exception("Sequence contains a number beyond integer values.");
+                            }
+                        }
+                    }
+
+                    var result = String.Join(", ", sequence.ToArray());
+                    var values = sequence.ToArray();
+                    chart1.Titles.Clear();
+                    string title = string.Concat(" steps to reach base cycle: ", steps.ToString());
+                    chart1.Titles.Add(title);
+                    textBox3.Text = result;
+
+                    /// binds new dataset
+                    chart1.DataSource = values;
+                    int[] x = Enumerable.Range(0, values.Length).ToArray();
+                    int[] y = sequence.ToArray();
+                    chart1.Series[0].Points.DataBindXY(x, y);
+                    chart1.Update();
+                }
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            /// gives curosr y value in an effort to help clearly show graph values
+            double XVal = chart1.ChartAreas[0].CursorY.Position;
+            label3.Text = XVal.ToString();
+        }
+    }
+}
